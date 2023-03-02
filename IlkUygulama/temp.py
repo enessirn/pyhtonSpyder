5e75497b4ae9479d9418d4a525c99880a83f77ba
Enes Sirin <sefsirin55@gmail.com>
2023-03-02 12:22:04 +0300

    first commit

---
+++ IlkUygulama/temp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# aciklama satiri
+import tkinter as tk
+ilkekran = tk.Tk()
+
+ilkekran.title('Form başlığı')
+label1 = tk.Label(text = 'bu bir etiket Label', fg = 'green', bg="black", font="Times New Roman")
+
+label1.pack()
+ilkekran.mainloop()
